af3e681c1319a575f70dcafc6ed0bc833d680f4d
anisur2805 <anisur2805@gmail.com>
2021-09-28 22:20:55 +0600

    [fix] styles and update close icon

--- includes/Frontend.php
+++ includes/Frontend.php
@@ -18,7 +18,7 @@ class Frontend {
         $options = get_option( 'awesome_popup_content' );
         ob_start();
         $html = '';
-        $html .= '<div class="awesome_popup_wrapper"><div class="awesome_popup_content"><span class="close_awesome_popup dashicons-before dashicons-no-alt">X</span>' . $atts['content'] . '</div></div>';
+        $html .= '<div class="awesome_popup_wrapper"><div class="awesome_popup_content"><span class="close_awesome_popup">X</span>' . $atts['content'] . '</div></div>';
         $clean_obj = ob_get_clean();
         return $html . $clean_obj;
         
